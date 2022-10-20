9d5667a1cc0c7a0567c48699c587abb33e734b02
kwibisam <kwibisam04@gmail.com>
2022-10-20 23:11:09 +0200

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">emart</a>
+      </header>
+      <main>List products here</main>
+    </div>
+  );
+}
+
+export default App;
