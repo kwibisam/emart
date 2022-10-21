290b40b59c5d1a6f89defec17261ad952da78d3f
kwibisam <kwibisam04@gmail.com>
2022-10-21 11:10:32 +0200

    fetch data from backed api

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from './data';
+// import data from './data';
+import axios from 'axios';
 
 export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div key={product.slug} className="product">
             <Link to={`/products/${product.slug}`}>
               <img src={product.image} alt={product.name} />
