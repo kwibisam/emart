f862080b481e476295959d384c8482bdca79b65e
kwibisam <kwibisam04@gmail.com>
2022-10-21 22:03:24 +0200

    added bootstrap 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,35 @@ import data from './data';
 import HomePage from './HomePage';
 import ProductPage from './ProductPage';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">emart</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products/:slug" element={<ProductPage />} />
-        </Routes>
-      </main>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Emart</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/products/:slug" element={<ProductPage />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
